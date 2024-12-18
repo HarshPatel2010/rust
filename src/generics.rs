f0ee9a6cff205646b0c2df780e1fe2bc99d4a3c9
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-18 16:32:30 +0530

    implementing generics

--- src/generics.rs
+++ src/generics.rs
@@ -11,7 +11,12 @@ fn gen_spec_t(_s: SGen<A>) {}
 fn gen_spec_i32(_s: SGen<i32>) {}
 
 fn generic<T>(_s: SGen<T>) {}
-
+pub fn sum<T: std::ops::Add<Output = T>+ std::fmt::Display>(a:T,b:T)->T{
+    let  result ;
+    result = a+b;
+    println!("{}", result);
+    result
+}
 pub fn main() {
     // Using the non-generic functions
     reg_fn(S(A));          // Concrete type.
@@ -23,6 +28,11 @@ pub fn main() {
 
     // Implicitly specified type parameter `char` to `generic()`.
     generic(SGen(7.7));
+
+    println!("Success!");
+    assert_eq!(5, sum(2i8, 3i8));
+    assert_eq!(50, sum(20, 30));
+    assert_eq!(2.46, sum(1.23, 1.23));
 
     println!("Success!");
 }
