447017dd8cb4f15f9790b7b75595a0017f679c4a
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-20 12:33:14 +0530

    traits done

--- src/traitBound.rs
+++ src/traitBound.rs
@@ -1,6 +1,43 @@
 pub fn main() {
     assert_eq!(sum(1, 2), 3);
     println!("{}", sum(5, 5));
+
+
+    // FIX the errors.
+    #[derive(Debug)]
+    struct Pair<T> {
+        x: T,
+        y: T,
+    }
+
+    impl<T> Pair<T> {
+        fn new(x: T, y: T) -> Self {
+            Self {
+                x,
+                y,
+            }
+        }
+    }
+
+    impl<T: std::fmt::Debug + PartialOrd> Pair<T> {
+        fn cmp_display(&self) {
+            if self.x >= self.y {
+                println!("The largest member is x = {:?}", self.x);
+            } else {
+                println!("The largest member is y = {:?}", self.y);
+            }
+        }
+    }
+#[derive(Debug,PartialEq,PartialOrd)]
+    struct Unit(i32);
+
+    fn main() {
+        let pair:Pair<Unit> = Pair::new(Unit(11),Unit(2));
+
+
+        pair.cmp_display();
+    }
+    main()
 }
 
 // Implement `fn sum` with trait bound in two ways.
