af41a00836d6835f6fa5ee71a50f39cc5b4f0e37
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-20 15:45:57 +0530

    dyn dispatch and static dispatch

--- src/dynBox.rs
+++ src/dynBox.rs
@@ -27,6 +27,39 @@ pub fn main() {
     draw_with_ref(&y);
 
     println!("Success!");
+
+    trait Foo {
+        fn method(&self) -> String;
+    }
+
+    impl Foo for u8 {
+        fn method(&self) -> String { format!("u8: {}", *self) }
+    }
+
+    impl Foo for String {
+        fn method(&self) -> String { format!("string: {}", *self) }
+    }
+
+    // IMPLEMENT below with generics.
+    fn static_dispatch<T:Foo>(a:T){
+        println!("{}", a.method());
+    }
+
+    // Implement below with trait objects.
+    fn dynamic_dispatch(a:&dyn Foo){
+        println!("{}", a.method());
+    }
+
+    fn main() {
+        let x = 5u8;
+        let y = "Hello".to_string();
+
+        static_dispatch(x);
+        dynamic_dispatch(&y);
+
+        println!("Success!");
+    }
+    main()
 }
 
 pub fn draw_with_box(x: Box<dyn Draw>) {
