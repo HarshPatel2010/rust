43d61fb097d4710d51d768be6214aba314971fd1
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-23 18:11:10 +0530

    string manipulation

--- src/stringgg.rs
+++ src/stringgg.rs
@@ -12,6 +12,23 @@ pub fn main() {
     assert_eq!(s, "hello, world!");
 
     println!("Success!");
+    // FILL in the blanks
+    fn main() {
+        let mut s:String = String::from("hello, world");
+
+        let slice1: &str = s.as_str(); // In two ways
+        assert_eq!(slice1, "hello, world");
+
+        let slice2:&str = &s[..5];
+        assert_eq!(slice2, "hello");
+
+        let slice3: &mut String = &mut s;
+        slice3.push('!');
+        assert_eq!(slice3, "hello, world!");
+
+        println!("Success!");
+    }
+    main()
 }
 
 pub fn move_ownership(s: String) {
