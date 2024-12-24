b5c9a006a11d63e48cf05d10890ec763fc7d0613
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-24 13:31:16 +0530

    x into vector manipulation

--- src/vectorr.rs
+++ src/vectorr.rs
@@ -67,6 +67,28 @@ pub fn main() {
             assert_eq!(v4, vec![0; 10]);
 
             println!("Success!");
+
+            // FIX the error and IMPLEMENT the code
+            fn main() {
+                let mut v = Vec::from([1, 2, 3]);
+                for i in 0..5 {
+                    println!("{:?}", v.get(i));
+                }
+
+                for i in 0..5 {
+                    // IMPLEMENT the code here...
+                    match v.get(i){
+                        Some(e)=>v[i]=e+1,
+                        None => v.push(i+2)
+
+                    }
+                }
+
+                assert_eq!(v, vec![2, 3, 4, 5, 6]);
+
+                println!("Success!");
+            }
+            main()
         }
         main()
     }
