e83c3e0eb5f9cb78f8747207769a1a60c5794a3a
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-24 18:57:26 +0530

    Hashmap operation

--- src/hashmap.rs
+++ src/hashmap.rs
@@ -24,4 +24,27 @@ pub fn main() {
     for (name, score) in scores {
         println!("The score of {} is {}", name, score);
     }
+
+    use std::collections::HashMap;
+    fn main() {
+        let teams:[(&str,i32);3] = [
+            ("Chinese Team", 100),
+            ("American Team", 10),
+            ("France Team", 50),
+        ];
+
+        let mut teams_map1 = HashMap::new();
+        for team in &teams {
+            teams_map1.insert(team.0, team.1);
+        }
+
+        // IMPLEMENT team_map2 in two ways
+        // Tips: one of the approaches is to use `collect` method
+        let teams_map2:HashMap<&str,i32> =teams.into_iter().collect();
+
+        assert_eq!(teams_map1, teams_map2);
+
+        println!("Success!");
+    }
+    main()
 }
