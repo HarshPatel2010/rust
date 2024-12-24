f2480d24d464d6d3d880f2bdc63085e894e22e7f
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-24 13:10:43 +0530

    vector collection extend

--- src/vectorr.rs
+++ src/vectorr.rs
@@ -23,6 +23,21 @@ pub fn main() {
     assert_eq!(v, v1);
 
     println!("Success!2");
+
+    // FILL in the blank
+    fn main() {
+        let mut v1 :Vec<i32>= Vec::from([1, 2, 4]);
+        v1.pop();//[1,2]
+        v1.push(3);//[1,2,3]
+
+        let mut v2 = Vec::new();
+        v2.extend(&v1);
+
+        assert_eq!(v1, v2);
+
+        println!("Success!");
+    }
+    main()
 }
 
 fn is_vec(v: &Vec<u8>) {}
