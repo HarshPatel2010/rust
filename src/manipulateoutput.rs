a25386ea136255cb1d5c11aa9b8a355d6cbf08a7
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-25 15:32:23 +0530

    parsing

--- src/manipulateoutput.rs
+++ src/manipulateoutput.rs
@@ -20,4 +20,17 @@ pub fn main() {
     println!("{}",origin);
 
     println!("Success!");
+    // To use `from_str` method, you need to introduce this trait into the current scope.
+
+    use std::str::FromStr;
+    fn main() {
+        let parsed: i32 = "5".parse().unwrap();
+        let turbo_parsed:i32 = "10".parse::<i32>().unwrap();
+        let from_str = i32::from_str("20").unwrap();
+        let sum = parsed + turbo_parsed + from_str;
+        assert_eq!(sum, 35);
+
+        println!("Success!");
+    }
+    main()
 }
