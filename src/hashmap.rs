6a69bf9c0a6a2c3968f7544872a9be4838ab3b3d
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-25 10:51:35 +0530

    Hashmap iteration

--- src/hashmap.rs
+++ src/hashmap.rs
@@ -71,6 +71,40 @@ pub fn main() {
             assert_eq!(*health, 50);
 
             println!("Success!");
+
+            // FIX the errors
+            // Tips: `derive` is usually a good way to implement some common used traits
+            use std::collections::HashMap;
+#[derive(Debug,Hash,Eq,PartialEq)]
+            struct Viking {
+                name: String,
+                country: String,
+            }
+
+            impl Viking {
+                /// Creates a new Viking.
+                fn new(name: &str, country: &str) -> Viking {
+                    Viking {
+                        name: name.to_string(),
+                        country: country.to_string(),
+                    }
+                }
+            }
+
+            fn main() {
+                // Use a HashMap to store the vikings' health points.
+                let vikings :HashMap<Viking,i32>= HashMap::from([
+                    (Viking::new("Einar", "Norway"), 25),
+                    (Viking::new("Olaf", "Denmark"), 24),
+                    (Viking::new("Harald", "Iceland"), 12),
+                ]);
+
+                // Use derived implementation to print the status of the vikings.
+                for (viking, health) in &vikings {
+                    println!("{:?} has {} hp", viking, health);
+                }
+            }
+            main()
         }
 
         fn random_stat_buff() -> u8 {
