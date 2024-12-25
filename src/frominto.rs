01b30b516a159598e3a62a1ff83665a48b3e372b
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-25 12:45:34 +0530

    error handling using from

--- src/frominto.rs
+++ src/frominto.rs
@@ -39,6 +39,43 @@ pub fn main() {
         assert_eq!(num.value, 30);
 
         println!("Success!");
+
+        //File --------------------------
+        use std::fs;
+        use std::io;
+        use std::num;
+
+        enum CliError {
+            IoError(io::Error),
+            ParseError(num::ParseIntError),
+        }
+
+        impl From<io::Error> for CliError {
+            // IMPLEMENT from method
+            fn from(e:io::Error)->Self{
+              CliError::IoError(e)
+            }
+        }
+
+        impl From<num::ParseIntError> for CliError {
+            // IMPLEMENT from method
+            fn from(e:num::ParseIntError)->Self{
+                CliError::ParseError(e)
+            }
+        }
+
+        fn open_and_parse_file(file_name: &str) -> Result<i32, CliError> {
+            // ? automatically converts io::Error to CliError
+            let contents:String = fs::read_to_string(&file_name)?;
+            // num::ParseIntError -> CliError
+            let num: i32 = contents.trim().parse()?;
+            Ok(num)
+        }
+
+        fn main() {
+            println!("Success!---");
+        }
+        main()
     }
     main()
 }
