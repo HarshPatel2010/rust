5e4ea26d80dbd801fb233096488e6debdcaa85b7
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-26 11:08:36 +0530

    Result with ? in file

--- src/result.rs
+++ src/result.rs
@@ -29,6 +29,38 @@ pub fn main() {
     fn main() {
         assert_eq!(multiply("3", "4").unwrap(), 12);
         println!("Success!, {}", multiply("3", "4").unwrap());
+
+        use std::fs::File;
+        use std::io::{self, Read};
+
+        fn read_file1() -> Result<String, io::Error> {
+            let f:Result<File,io::Error> = File::open("hello.txt");
+            let mut f :File= match f {
+                Ok(file) => file,
+                Err(e) => return Err(e),
+            };
+
+            let mut s :String= String::new();
+            match f.read_to_string(&mut s) {
+                Ok(_) => Ok(s),
+                Err(e) => Err(e),
+            }
+        }
+
+        // FILL in the blanks with one code line
+        // DON'T change any code lines
+        fn read_file2() -> Result<String, io::Error> {
+            let mut s :String= String::new();
+
+            File::open("hello.txt")?.read_to_string(&mut s)?;
+            Ok(s)
+        }
+
+        fn main() {
+            assert_eq!(read_file1().unwrap_err().to_string(), read_file2().unwrap_err().to_string());
+            println!("Success!");
+        }
+        main()
     }
     main()
 }
