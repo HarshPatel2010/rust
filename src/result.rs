c56dc3c4a469ef3a5c8ff9986835d53701be1a9e
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-26 10:54:53 +0530

    Result with ?

--- src/result.rs
+++ src/result.rs
@@ -15,5 +15,20 @@ pub fn main() {
     let result = multiply("4", "2");
     assert_eq!(result.unwrap(), 8);
 
-    println!("Success123!");
+
+    use std::num::ParseIntError;
+
+    // IMPLEMENT multiply with ?
+    // DON'T use unwrap here
+    fn multiply(n1_str: &str, n2_str: &str) -> Result<i32,ParseIntError> {
+       let n1:i32 = n1_str.parse::<i32>()?;
+        let n2:i32 = n2_str.parse::<i32>()?;
+        Ok(n1*n2)
+    }
+
+    fn main() {
+        assert_eq!(multiply("3", "4").unwrap(), 12);
+        println!("Success!, {}", multiply("3", "4").unwrap());
+    }
+    main()
 }
