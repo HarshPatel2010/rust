67e88f41c54a43c0786c71cbd9e22a1dc5f88452
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-26 11:16:55 +0530

    map, and then implemented

--- src/result.rs
+++ src/result.rs
@@ -59,6 +59,21 @@ pub fn main() {
         fn main() {
             assert_eq!(read_file1().unwrap_err().to_string(), read_file2().unwrap_err().to_string());
             println!("Success!");
+            use std::num::ParseIntError;
+
+            // FILL in the blank in two ways: map, and then
+            fn add_two(n_str: &str) -> Result<i32, ParseIntError> {
+              //  n_str.parse::<i32>().map(|n| n + 2)
+                n_str.parse::<i32>().and_then(|n| Ok(n + 2))
+
+            }
+
+            fn main() {
+                assert_eq!(add_two("4").unwrap(), 6);
+
+                println!("Success! {}",add_two("4").unwrap());
+            }
+            main()
         }
         main()
     }
