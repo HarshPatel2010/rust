bd9ddd64405f6967a27380ed41399a2dbd03c01b
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-27 15:41:27 +0530

    Lifetime static type

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -115,7 +115,22 @@ pub fn main() {
                         }
                     }
 
-                    fn main() {}
+                    fn main() {
+                        fn main() {
+                            let static_string = "I'm in read-only memory";
+
+                            {
+                                // Make a `string` literal and print it:
+                                println!("static_string: {}", static_string);
+
+                                // When `static_string` goes out of scope, the reference
+                                // can no longer be used, but the data remains in the binary.
+                            }
+
+                            println!("static_string reference remains alive: {}", static_string);
+                        }
+                        main()
+                    }
                     main()
                 }
                 main()
