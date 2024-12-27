3b82624323326b696cb05fdea5dea1465506f7c1
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-27 13:55:51 +0530

    Lifetime practice

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -65,6 +65,35 @@ pub fn main() {
         println!("x and y are borrowed in {:?}", double);
         println!("x is borrowed in {:?}", reference);
         println!("y is *not* borrowed in {:?}", number);
+        /* Make it work */
+
+        #[derive(Debug)]
+        struct NoCopyType {}
+
+        #[derive(Debug)]
+        struct Example<'a, 'b> {
+            a: &'a u32,
+            b: &'b NoCopyType
+        }
+
+        fn main()
+        {
+            /* 'a tied to fn-main stackframe */
+            let var_a = 35;
+            let example: Example;
+
+            {
+                /* Lifetime 'b tied to new stackframe/scope */
+                let var_b = NoCopyType {};
+
+                /* fixme */
+                example = Example { a: &var_a, b: &var_b };
+                println!("(Success!) {:?}", example);
+
+            }
+
+        }
+        main()
     }
     main()
 }
