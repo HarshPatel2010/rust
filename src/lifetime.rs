f9bfb97fb5883f9dc2300b8ac171c50b2ec652a9
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-27 14:05:59 +0530

    Lifetime on method

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -89,7 +89,23 @@ pub fn main() {
                 /* fixme */
                 example = Example { a: &var_a, b: &var_b };
                 println!("(Success!) {:?}", example);
+                struct Owner(i32);
 
+                impl Owner {
+                    // Annotate lifetimes as in a standalone function.
+                    fn add_one<'a>(&'a mut self) { self.0 += 1; }
+                    fn print<'a>(&'a self) {
+                        println!("`print`: {}", self.0);
+                    }
+                }
+
+                fn main() {
+                    let mut owner = Owner(18);
+
+                    owner.add_one();
+                    owner.print();
+                }
+                main()
             }
 
         }
