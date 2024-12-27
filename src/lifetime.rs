c2ae18db2c42d405ce8421337941834507ade983
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-27 18:03:13 +0530

    closure fn

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -159,6 +159,20 @@ pub fn main() {
                                         let x = 1;
                                         let closure = |val| val + x;
                                         assert_eq!(closure(2), 3);
+                                        /* Make it work by changing the trait bound, in two ways*/
+                                        fn fn_once<F>(func: F)
+                                        where
+                                            F: Fn(usize) -> bool,
+                                        {
+                                            println!("{}", func(3));
+                                            println!("{}", func(4));
+                                        }
+
+                                        fn main() {
+                                            let x = vec![1, 2, 3];
+                                            fn_once(|z|{z == x.len()})
+                                        }
+                                        main()
                                     }
                                     main()
                                 }
