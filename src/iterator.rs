7bfed5857e8719434dcc635eaa3c50325e806f3b
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-28 13:21:15 +0530

    iterator iter func

--- src/iterator.rs
+++ src/iterator.rs
@@ -12,6 +12,16 @@ Using two ways if possible */
         assert_eq!(v1.next(),Some(1));
         assert_eq!(v1.next(), Some(2));
         assert_eq!(v1.next(), None);
+        /* Make it work */
+        fn main() {
+            let arr:Vec<i32> = vec![0; 10];
+            for i in arr.iter() {
+                println!("{}", i);
+            }
+
+            println!("{:?}",arr);
+        }
+        main()
     }
     main()
 }
