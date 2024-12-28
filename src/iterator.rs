2ea978efae99fa459ecb7f2600a1da92e088ae9d
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-28 13:18:39 +0530

    iterator into_iter func

--- src/iterator.rs
+++ src/iterator.rs
@@ -2,5 +2,16 @@ pub fn main() {
     let v = vec![1, 2, 3];
     for x in v {
         println!("{}",x)
+
     }
+    /* Fill the blanks and fix the errors.
+Using two ways if possible */
+    fn main() {
+        let mut v1 = vec![1, 2].into_iter();
+
+        assert_eq!(v1.next(),Some(1));
+        assert_eq!(v1.next(), Some(2));
+        assert_eq!(v1.next(), None);
+    }
+    main()
 }
