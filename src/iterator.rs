2c59ff307eb9cf87fa25448b07ee3ecd2e71c945
HarshPatel2010 <harshpatel29301@gmail.com>
2024-12-28 15:34:54 +0530

    iterator adaptors

--- src/iterator.rs
+++ src/iterator.rs
@@ -45,6 +45,15 @@ Using two ways if possible */
                     for e in values  {
                         println!("{}",e);
                     }
+                    /* Fill in the blanks */
+                    fn main() {
+                        let v1: Vec<i32> = vec![1, 2, 3];
+
+                        let v2: Vec<i32> = v1.iter().map(|n| n+1).collect();
+
+                        assert_eq!(v2, vec![2, 3, 4]);
+                    }
+                    main()
                 }
                 main()
             }
